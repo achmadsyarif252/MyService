2c231567487f04d4cd0c8faefcea8398b5972097
achmad2002 <19kaizen@gmail.com>
2022-12-28 11:42:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myservice
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var serviceIntent = Intent(this, MyBackgroundService::class.java)
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
+        }
+    }
+
+
+}
