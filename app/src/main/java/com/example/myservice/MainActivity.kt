3580e568f179503c932482bff1361a85965e2165
achmad2002 <19kaizen@gmail.com>
2022-12-28 13:03:52 +0700

    menambahkan bound service

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.example.myservice.databinding.ActivityMainBinding
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var boundStatus = false
+    private lateinit var boudService: MyBoundService
+
+    private val conection = object : ServiceConnection {
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            boundStatus = false
+        }
+
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            val myBinder = p1 as MyBoundService.MyBinder
+            boudService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+
+    }
+
+    private fun getNumberFromService() {
+        boudService.numberLiveData.observe(this) { number ->
+            binding.tvBoundServiceNumber.text = number.toString()
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +63,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnStopForegroundService.setOnClickListener {
             stopService(foregroundServiceIntent)
+        }
+
+        val boundServiceIntent = Intent(this, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, conection, BIND_AUTO_CREATE)
+        }
+
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(conection)
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (boundStatus) {
+            unbindService(conection)
+            boundStatus = false
         }
     }
 
